a81d8b3a8fd45685e08e7cc540f57992b1dfb87b
Loic <loic.florimont@ecole-isitech.fr>
2023-06-15 16:04:14 +0200

    3st Commit

---
+++ combat.hpp
@@ -0,0 +1,13 @@
+//
+//  combat.hpp
+//  cpp_tpfinal
+//
+//  Created by Loic FLORIMONT on 15/06/2023.
+//
+
+#ifndef combat_hpp
+#define combat_hpp
+
+#include <stdio.h>
+
+#endif /* combat_hpp */
--- cpp_tpfinal/main.cpp
+++ cpp_tpfinal/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+
 class Boxeur {
 private:
     std::string nom;
@@ -8,12 +9,13 @@ private:
 
 public:
     Boxeur(const std::string& nom, float poids) : nom(nom), poids(poids) {
-        std::cout << "Boxeur 1 : " << nom << std::endl;
+        std::cout << "Participant : " << std::endl;
         std::cout << "" << std::endl;
     }
 
     ~Boxeur() {
         std::cout << "Destructeur Boxeur : " << nom << std::endl;
+        std::cout << "" << std::endl;
     }
 
     std::string getNom() const {
@@ -30,16 +32,19 @@ public:
 };
 
 int main() {
-    // Instanciation de manière automatique
     Boxeur boxeur_1("Adonis Creed", 75.0f);
-    std::cout << "Adresse : " << "365 W END AVE NEW YORK NY 10024-6560 USA" << std::endl;
+    std::cout << "Nom : " << boxeur_1.getNom() << std::endl;
     std::cout << "Poids : " << boxeur_1.getPoids() << "  kg" << std::endl;
+    std::cout << "Adresse : " << &boxeur_1 << std::endl;
+    std::cout << "" << std::endl;
 
-    // Instanciation de manière dynamique
-    Boxeur* boxeur_2 = new Boxeur("Box_2", 78.0f);
-    std::cout << "Adresse boxeur_2 : " << boxeur_2 << std::endl;
-    std::cout << "Nom boxeur_2 : " << boxeur_2->getNom() << std::endl;
-    std::cout << "Poids boxeur_2 : " << boxeur_2->getPoids() << " kg" << std::endl;
+
+    Boxeur* boxeur_2 = new Boxeur("Rocky Balboa", 80.0f);
+    std::cout << "Nom : " << boxeur_2->getNom() << std::endl;
+    std::cout << "Poids : " << boxeur_2->getPoids() << " kg" << std::endl;
+    std::cout << "Adresse : " << &boxeur_2 << std::endl;
+    std::cout << "" << std::endl;
+
 
     delete boxeur_2;
 
